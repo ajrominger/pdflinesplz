4484d4a70f03641209dd2b8eedd01ed4cfb94e6e
ajrominger <ajrominger@gmail.com>
2021-09-21 13:07:39 -0400

    finalized r function and tex template

--- R/addLines2pdf.R
+++ R/addLines2pdf.R
@@ -13,11 +13,21 @@ addLines2pdf <- function(file) {
     # store key names
     pathName <- dirname(file)
     fileName <- basename(file)
+    usrName <- gsub('\\..*', '', fileName)
     scriptName <- 'findlines.sh'
     texName <- 'addLineTemplate.tex'
 
-    # make a temporary directory and copy everything there
-    tempd <- tempdir()
+    # make a temporary directory and make sure it's empty (an issue when
+    # re-running script without restarting R session)
+    tempd <- tempdir(check = TRUE)
+    whatThere <- file.path(tempd, list.files(tempd))
+    whatThere <- whatThere[!grepl('rs-graphics', whatThere)]
+
+    if(length(whatThere) > 0) {
+        file.remove(whatThere)
+    }
+
+    # copy all needed files to tempd
     file.copy(file, tempd)
 
     findlines <- system.file(scriptName, package = 'pdflinesplz')
@@ -29,10 +39,10 @@ addLines2pdf <- function(file) {
     # run the script in the temp dir
     # this script makes a text file for each page of the pdf with info on where
     # the lines are that are all white and where the lines are with text
-    x <- system(sprintf('cd %s && sh %s %s',
-                        tempd,
-                        scriptName,
-                        gsub('\\..*', '', fileName)))
+    system(sprintf('cd %s && sh %s %s',
+                   tempd,
+                   scriptName,
+                   usrName))
 
     # load all generated text files and process
     xf <- list.files(tempd, pattern = '-raw.txt')
@@ -46,11 +56,39 @@ addLines2pdf <- function(file) {
         # extract y coordinate of those pixels--this is where the lines are
         pix <- gsub('.*,|:', '', pix)
 
-        return(pix)
+        # make file name to save pixel indeces
+        fout <- gsub('-raw\\.txt', '.txt', f)
+
+        # save pixel indeces
+        writeLines(pix, fout)
+
+        # remove raw pixel files and raster files
+        file.remove(c(f, gsub('-raw\\.txt', '', f)))
+
+        # return file paths of clean txt files
+        return(fout)
     })
 
-    # add lines to tex template to print pdf pages
+    # add lines to Rmd file that will render pdf with tex template
+    add2rmd <- c('---',
+                 'output:',
+                 '    pdf_document:',
+                 '        template: addLineTemplate.tex',
+                 '---',
+                 '',
+                 sprintf('\\def\\pdfname{%s}', usrName),
+                 sprintf('\\addlinenumbers{%s}{20}{50}', 1:length(x)))
 
+    # write out the rmd file
+    rmdFile <- file.path(tempd, paste0(usrName, '_withLineNum.Rmd'))
+    writeLines(add2rmd, rmdFile)
 
-    return(x)
+    # render the rmd file
+    outpath <- rmarkdown::render(rmdFile, envir = new.env(parent = globalenv()))
+
+    # move new pdf with line numbers back to the directory given by the user
+    finalPath <- file.path(pathName, basename(outpath))
+    file.copy(outpath, finalPath)
+
+    invisible(finalPath)
 }
